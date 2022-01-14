0d53d1a5fca3da4a7c37b5b1847fe5b0efda7248
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2022-01-14 10:21:07 +0530

    Create Asycn_Await.js

---
+++ Asycn_Await.js
@@ -0,0 +1,18 @@
+//see in console how code is going 
+
+
+ async function pranjal(){
+     console.log("inside pranjal function");
+     const response=await fetch("https://api.github.com/users");
+     console.log("before response");
+     const user=await response.json();
+     console.log("user resolved");
+     return user ;
+    // return "pranjal";
+}
+ console.log("before calling function")
+let a=pranjal();
+console.log("after calling function")
+console.log(a)
+a.then(data=>console.log(data));
+console.log("last js line")
