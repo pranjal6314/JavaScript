2f9b3a74ad0d282f7ea0247011833e5a44a2c36b
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2022-01-15 09:20:02 +0530

    Update regular_Expression.js

--- regular_Expression.js
+++ regular_Expression.js
@@ -42,3 +42,42 @@ if(regex.test(str)){
 }else{
     console.log(`the string "${str}" does not matches ecpression ${regex}`)
 }
+
+
+
+
+console.log("***************************************************************");
+//character set
+let regg=/pra[a-z]jal/ //can be any character between a to z
+     regg=/pra[abn]jal/ //can be  a,b and n
+     regg=/pra[^abe]jal/ //can't be  a,b and e
+//Quantifier
+    regg=/go{2}d/
+    regg=/pranjala{0,2}/ //a can come exactly 0 to 2(0,1,2) times
+//grouping
+    regg=/(pa[0-9]){2}/
+let string="pranjal is6 a007 good  #$boy,pranjal leave in @rajasthan ,pa1pa2  333707999"
+let res=regg.exec(string);
+console.log(res)
+
+
+console.log("***************************************************************");
+//character class
+let reg2=/pra\wjal/ //word class it can be _ or alphabate or number
+    reg2=/\w+al/  //\w+ means one or more word character
+    reg2=/\Wleave/ //\W use for non-word character (space,@,#,%,$)
+    reg2=/\Wraj/ 
+    reg2=/\W+boy/
+    reg2=/\d999/ //digit which end with 999
+    reg2=/\d+/ //more then one digit
+    reg2=/\D007/  //non digit number
+    reg2=/\D+6/ //more then one
+    reg2=/\sgood/ //maatch white space character
+    reg2=/\s+/
+    reg2=/p(?=r)/ //after p there is a 'r'
+    reg2=/p(?!a)/ // there shoul be no 'a' after p
+
+
+let res2=reg2.exec(string)
+console.log(res2)
+
