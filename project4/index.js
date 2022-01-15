7827fe80b86ee8236c801d8c4ae57cd9bff64b39
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2022-01-15 22:34:09 +0530

    Create index.js

---
+++ project4/index.js
@@ -0,0 +1,8 @@
+//js
+
+function start(){
+    document.getElementById("success").style.display="none";
+}
+let btn=document.getElementById("close").addEventListener("onclick", e=>{
+    document.getElementById("success").style.display="none";
+})
